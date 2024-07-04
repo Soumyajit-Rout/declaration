ef6e8bf11f82990b663a1c2d41d1993f09e0e95d
Soumyajitsynk <soumyajit.rout@finloge.com>
2024-07-04 12:41:22 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo cp -rf warehouse_management.socket /etc/systemd/system/
-sudo cp -rf warehouse_management.service /etc/systemd/system/
+sudo cp -rf declaration-management.socket /etc/systemd/system/
+sudo cp -rf declaration-management.service /etc/systemd/system/
 
-sudo systemctl start warehouse_management.socket
-sudo systemctl enable warehouse_management.socket
-sudo systemctl status warehouse_management.socket
+sudo systemctl start declaration-management.socket
+sudo systemctl enable declaration-management.socket
+sudo systemctl status wdeclaration-management.socket
 
 sudo systemctl restart nginx
