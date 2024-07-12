54d686a080fd99829abab6c48570a3ab6c9ec29a
sreejesh <sreejesh@finloge.com>
2024-07-12 13:26:47 +0530

    changes the generics view to api view

--- declaration/urls.py
+++ declaration/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
      path('delete/<uuid:pk>/', delete_declaration, name='delete_declaration'),
      path('search-hscode/', search_hscode, name='search_hscode'),
      path('api/DeclarationRegistration/verification/getDeclarationRegistrationData',ListDeclarations.as_view(),name='list_declaration'),
-     path('api/DeclarationRegistration/verification/getDeclarationRegistrationDatabyId/<uuid:id>',RetrieveDeclaration.as_view(),name='retrieve_declaration'),
-     path('api/DeclarationRegistration/verification/updateDeclarationRegistrationData/<uuid:id>',UpdateDeclaration.as_view(),name='retrieve_declaration')
+     path('api/DeclarationRegistration/verification/getDeclarationRegistrationDatabyId',RetrieveDeclaration.as_view(),name='retrieve_declaration'),
+     path('api/DeclarationRegistration/verification/updateDeclarationRegistrationData',UpdateDeclaration.as_view(),name='retrieve_declaration')
 
 
 ]
