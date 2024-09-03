ba1d95a268c8ef28647fa2c7f5e7c6407a356a38
sreejesh <sreejesh@finloge.com>
2024-09-03 18:58:56 +0530

    Updates the design

--- DeclarationManagement/settings.py
+++ DeclarationManagement/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'declaration',
     'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
