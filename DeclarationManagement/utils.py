f853b0c4f7abffa36d23ea8869ec791848269e69
synkcodeparoop <praroop@finloge.com>
2024-09-12 18:06:57 +0530

    token change for declaration

--- DeclarationManagement/utils.py
+++ DeclarationManagement/utils.py
@@ -1,6 +1,7 @@
 # File contaning all the utility functions
 from django.conf import settings
 from urllib.request import Request
+from declaration.permissions import STATIC_API_TOKEN
 
 
 #To-Do Change it to dynamic as soon as auth functionality is decided
@@ -12,6 +13,6 @@ class Authentication:
         if not auth_header or not auth_header.startswith('Bearer '):
             return False
         token = auth_header.split(' ')[1]
-        if token != settings.STATIC_API_TOKEN:
+        if token != STATIC_API_TOKEN:
             return False
         return True
